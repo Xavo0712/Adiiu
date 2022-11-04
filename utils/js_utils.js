717fa09f14d7902ec3a9506e2a1b819d90435740
Xavo0712 <61181589+Xavo0712@users.noreply.github.com>
2022-11-04 15:26:30 +0100

    Add names on series charts

--- utils/js_utils.js
+++ utils/js_utils.js
@@ -21,20 +21,32 @@ function getComparisonDogsData(Breed1, Breed2) {
                 return dog.Breed == Breed1 || dog.Breed == Breed2;
             });
             if(dogs[0]){
+                $('#chart1').highcharts().series[0].setName(dogs[0].Breed);
                 $('#chart1').highcharts().series[0].setData([[dogs[0].Breed, parseInt(dogs[0].popularity)]]);
+                $('#chart2').highcharts().series[0].setName(dogs[0].Breed);
                 $('#chart2').highcharts().series[0].setData([[dogs[0].Breed, (parseInt(dogs[0].min_weight) + parseInt(dogs[0].max_weight))/2]]);
+                $('#chart3').highcharts().series[0].setName(dogs[0].Breed);
                 $('#chart3').highcharts().series[0].setData([[dogs[0].Breed, (parseInt(dogs[0].min_height) + parseInt(dogs[0].max_height))/2]]);
+                $('#chart4').highcharts().series[0].setName(dogs[0].Breed);
                 $('#chart4').highcharts().series[0].setData([[dogs[0].Breed, (parseInt(dogs[0].min_expectancy) + parseInt(dogs[0].max_expectancy))/2]]);
+                $('#chart5').highcharts().series[0].setName(dogs[0].Breed);
                 $('#chart5').highcharts().series[0].setData([[dogs[0].Breed, parseFloat(dogs[0].trainability_value)]]);
+                $('#chart6').highcharts().series[0].setName(dogs[0].Breed);
                 $('#chart6').highcharts().series[0].setData([[dogs[0].Breed, parseFloat(dogs[0].energy_level_value)]]);
             }
             
             if(dogs[1]) {
+                $('#chart1').highcharts().series[1].setName(dogs[1].Breed);
                 $('#chart1').highcharts().series[1].setData([[dogs[1].Breed, parseInt(dogs[1].popularity)]]);
+                $('#chart2').highcharts().series[1].setName(dogs[1].Breed);
                 $('#chart2').highcharts().series[1].setData([[dogs[1].Breed, (parseInt(dogs[1].min_weight) + parseInt(dogs[1].max_weight))/2]]);
+                $('#chart3').highcharts().series[1].setName(dogs[1].Breed);
                 $('#chart3').highcharts().series[1].setData([[dogs[1].Breed, (parseInt(dogs[1].min_height) + parseInt(dogs[1].max_height))/2]]);
+                $('#chart4').highcharts().series[1].setName(dogs[1].Breed);
                 $('#chart4').highcharts().series[1].setData([[dogs[1].Breed, (parseInt(dogs[1].min_expectancy) + parseInt(dogs[1].max_expectancy))/2]]);
+                $('#chart5').highcharts().series[1].setName(dogs[1].Breed);
                 $('#chart5').highcharts().series[1].setData([[dogs[1].Breed, parseFloat(dogs[1].trainability_value)]]);
+                $('#chart6').highcharts().series[1].setName(dogs[1].Breed);
                 $('#chart6').highcharts().series[1].setData([[dogs[1].Breed, parseFloat(dogs[1].energy_level_value)]]);
             }
         }
