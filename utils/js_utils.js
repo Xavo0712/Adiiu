6e5711826ebf5a72ed8bd34a20f4ac8be674d720
Xavier <xavierpalouoliver@gmail.com>
2022-11-05 16:41:02 +0100

    errors favourite

--- utils/js_utils.js
+++ utils/js_utils.js
@@ -53,6 +53,32 @@ function getComparisonDogsData(Breed1, Breed2) {
     });
 }
 
+function getFavouriteDogsData(Breed1) {
+    $.ajax({
+        type: "GET",
+        url: "/Adiiu/utils/getDogs.php",
+        async: true,
+        success: function(data) {
+            jsonData = JSON.parse(data);
+            dogs = jsonData.filter(function(dog) {
+                return dog.Breed == Breed1;
+            });
+            if(dogs[0]){
+                $('#chart1').highcharts().series[0].setName(dogs[0].Breed);
+                $('#chart1').highcharts().series[0].setData([[dogs[0].Breed, parseInt(dogs[0].popularity)]]);
+                $('#chart2').highcharts().series[0].setName(dogs[0].Breed);
+                $('#chart2').highcharts().series[0].setData([[dogs[0].Breed, (parseInt(dogs[0].min_weight) + parseInt(dogs[0].max_weight))/2]]);
+                $('#chart3').highcharts().series[0].setName(dogs[0].Breed);
+                $('#chart3').highcharts().series[0].setData([[dogs[0].Breed, (parseInt(dogs[0].min_height) + parseInt(dogs[0].max_height))/2]]);
+                $('#chart4').highcharts().series[0].setName(dogs[0].Breed);
+                $('#chart4').highcharts().series[0].setData([[dogs[0].Breed, (parseInt(dogs[0].min_expectancy) + parseInt(dogs[0].max_expectancy))/2]]);
+                $('#chart5').highcharts().series[0].setName(dogs[0].Breed);
+                $('#chart5').highcharts().series[0].setData([[dogs[0].Breed, parseFloat(dogs[0].trainability_value)]]);
+            }
+        }
+    });
+}
+
 function getAllDogsData(callback) {
     $.ajax({
         type: "GET",
--- views/favourite.php
+++ views/favourite.php
@@ -1,8 +1,237 @@
-<?php 
+<?php
 require_once __DIR__ . "/../head.php";
 require_once __DIR__ . "../header.php";
+
+require_once __DIR__ . "/../utils/db_utils.php";
+/* Getting demo_viewer table data */
+$sql = "SELECT * FROM mytable";
+$dogs = mysqli_query($mysqli, $sql);
 ?>
 
 <body class="mainBody">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12" style="margin-top: 25px;">
+                <select id="dogBreed1" class="form-select">
+                    <option value="" selected>Choose a breed</option>
+                    <?php
+                    foreach($dogs as $dog) {
+                        echo '<option value="'. $dog["Breed"] .'">'. $dog["Breed"] .'</option>';
+                    }
+                    ?>
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <div class="container favouriteContainer">
+        <div class="div1" id="chart1"></div>
+        <div class="div2" id="chart2"></div>
+        <div class="div3" id="chart3"></div>
+        <div class="div4" id="chart4"></div>
+        <div class="div5" id="chart5"></div>
+        <div class="div6" id="chart6"></div>
+    </div>
 
 </body>
+
+<script>
+    $(document).ready(function() {
+        $('#headerTitle').text("Comparison tool");
+        $('#headerImg').attr("src", "./../imgs/compareDog.png");
+        var select1 = document.querySelector('#dogBreed1');
+        dselect(select1, {
+            search: true
+        });
+
+        $('#dogBreed1').change(function() {
+            debugger;
+            getFavouriteDogsData($('#dogBreed1').val());
+        });
+
+
+        $('#chart1').highcharts({
+            chart: {
+                backgroundColor: '#FFF5EB',
+                type: "column",
+                borderColor: "#333333",
+                borderRadius: 5,
+                borderWidth: 5,
+
+            },
+            title: {
+                text: "Popularity Comparison"
+            },
+            xAxis: {
+                allowDecimals: false,
+                title: {
+                    text: "Breed"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Popularity"
+                }
+
+            },
+            colors: ['#82A8D2', '#6AF9C4', '#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+                '#FF9655', '#FFF263'
+            ],
+            series: [{}, {}],
+            credits: false
+        });
+
+        $('#chart2').highcharts({
+            chart: {
+                backgroundColor: '#FFF5EB',
+                type: "column",
+                borderColor: "#333333",
+                borderRadius: 5,
+                borderWidth: 5,
+
+            },
+            title: {
+                text: "Mean Weight Comparison"
+            },
+            xAxis: {
+                allowDecimals: false,
+                title: {
+                    text: "Breed"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Mean Weight"
+                }
+
+            },
+            colors: ['#82A8D2', '#6AF9C4', '#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+                '#FF9655', '#FFF263'
+            ],
+            series: [{}, {}],
+            credits: false
+        });
+
+        $('#chart3').highcharts({
+            chart: {
+                backgroundColor: '#FFF5EB',
+                type: "column",
+                borderColor: "#333333",
+                borderRadius: 5,
+                borderWidth: 5,
+
+            },
+            title: {
+                text: "Mean Height Comparison"
+            },
+            xAxis: {
+                allowDecimals: false,
+                title: {
+                    text: "Breed"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Mean Height"
+                }
+
+            },
+            colors: ['#82A8D2', '#6AF9C4', '#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+                '#FF9655', '#FFF263'
+            ],
+            series: [{}, {}],
+            credits: false
+        });
+
+
+        $('#chart4').highcharts({
+            chart: {
+                backgroundColor: '#FFF5EB',
+                type: "column",
+                borderColor: "#333333",
+                borderRadius: 5,
+                borderWidth: 5,
+            },
+            title: {
+                text: "Mean Expectancy Comparison"
+            },
+            xAxis: {
+                allowDecimals: false,
+                title: {
+                    text: "Breed"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Mean Expectancy"
+                }
+
+            },
+            colors: ['#82A8D2', '#6AF9C4', '#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+                '#FF9655', '#FFF263'
+            ],
+            series: [{}, {}],
+            credits: false
+        });
+
+        $('#chart5').highcharts({
+            chart: {
+                backgroundColor: '#FFF5EB',
+                type: "column",
+                borderColor: "#333333",
+                borderRadius: 5,
+                borderWidth: 5,
+            },
+            title: {
+                text: "Trainability Level Comparison"
+            },
+            xAxis: {
+                allowDecimals: false,
+                title: {
+                    text: "Breed"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Trainability Level"
+                }
+
+            },
+            colors: ['#82A8D2', '#6AF9C4', '#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+                '#FF9655', '#FFF263'
+            ],
+            series: [{}, {}],
+            credits: false
+        });
+
+        $('#chart6').highcharts({
+            chart: {
+                backgroundColor: '#FFF5EB',
+                type: "column",
+                borderColor: "#333333",
+                borderRadius: 5,
+                borderWidth: 5,
+            },
+            title: {
+                text: "Energy Level Comparison"
+            },
+            xAxis: {
+                allowDecimals: false,
+                title: {
+                    text: "Breed"
+                }
+            },
+            yAxis: {
+                title: {
+                    text: "Energy Level"
+                }
+
+            },
+            colors: ['#82A8D2', '#6AF9C4', '#058DC7', '#50B432', '#ED561B', '#DDDF00', '#24CBE5', '#64E572',
+                '#FF9655', '#FFF263'
+            ],
+            series: [{}, {}],
+            credits: false
+        });
+    });
+</script>
